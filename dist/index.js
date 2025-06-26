d673828356edb4dc7b67a29545ea602a2a90da2e
ViktorJJF <vj.jimenez96@gmail.com>
2025-06-25 22:23:32 -0500

    build

--- dist/index.js
+++ dist/index.js
@@ -13,7 +13,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.socketService = void 0;
-require("dotenv/config");
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const morgan_1 = __importDefault(require("morgan"));
